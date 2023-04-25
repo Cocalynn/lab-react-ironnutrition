e389a9942cbc601b1d073cd51c8aa87cc66d8a33
YinongLi <yinong.li97@gmail.com>
2023-04-25 10:44:59 +0800

    optimize form

--- src/components/AddFoodForm.js
+++ src/components/AddFoodForm.js
@@ -4,33 +4,38 @@ import { useState } from 'react';
 
 function AddFoodForm(props) {
 
-    const [name, setName] = useState('')
-    const [image, setImage] = useState('')
-    const [calories, setCalories] = useState()
-    const [servings, setServings] = useState()
+
+    const [state, setState] = useState({
+        name: '',
+        image: '',
+        calories: null,
+        servings: null
+    })
+
     const [display, setDisplay] = useState(true)
 
-    const handleNameInput = e => { setName(e.target.value) }
-    const handleImageInput = e => { setImage(e.target.value) }
-    const handleCaloriesInput = e => { setCalories(e.target.value) }
-    const handleServingsInput = e => { setServings(e.target.value) }
+    //optimize the code by using one function to handle all inputs
+    const handleInput = (e) => {
+        const {name, value} = e.target
+        setState(state=>({...state, [name]: value}))
+        console.log(state)
+
+    }
+
 
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault()//prevent the page from refreshing
 
-        const newFood = {
-            name: name,
-            image: image,
-            calories: calories,
-            servings: servings
-        }
+        const newFood = state
         props.addFood(newFood)
 
-        // Clear the form
-        setName('')
-        setImage('')
-        setCalories('')
-        setServings('')
+        setState(
+        {
+            name: '',
+            image: '',
+            calories: null,
+            servings: null
+        })
     }
 
     
@@ -42,13 +47,13 @@ function AddFoodForm(props) {
             <>
                 <Divider>Add Food Entry</Divider>
                 <label>Name</label>
-                <Input value={name} type="text" onChange={handleNameInput} />
+                <Input name="name" value={state.name} type="text" onChange={handleInput} />
                 <label>Image</label>
-                <Input value={image} type="text" onChange={handleImageInput} />
+                <Input name="image" value={state.image} type="text" onChange={handleInput} />
                 <label>Calories</label>
-                <Input value={calories} type="number" onChange={handleCaloriesInput} />
+                <Input name="calories" value={state.calories} type="number" onChange={handleInput} />
                 <label>Servings</label>
-                <Input value={servings} type="number" onChange={handleServingsInput} />
+                <Input name="servings" value={state.servings} type="number" onChange={handleInput} />
                 <button type="submit">Create</button>
             </>
         }
